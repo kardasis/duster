f64d1d30f152d6c6fc77845679dcf2be9ce8726a
Ari Kardasis <ari.kardasis@gmail.com>
2022-07-03 10:24:38 -0400

    a few final rubocop things

--- app/controllers/api/runs_controller.rb
+++ app/controllers/api/runs_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-# Controller for runs
-class Api::RunsController < Api::ApiController
-  def create
-    run = Run.new
-    run.save!
-    render json: run
+module Api
+  class RunsController < Api::ApiController
+    def create
+      run = Run.new
+      run.save!
+      render json: run
+    end
   end
 end
