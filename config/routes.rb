8778811eee1b4bb761b96bd0de37271dcf03368f
Ari Kardasis <ari.kardasis@gmail.com>
2022-07-04 10:14:06 -0400

    change datapoint route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   namespace :api do
     resource :runs, only: %i[create show]
-    post 'datapoints/:run_id', to: 'datapoints#add'
+    post 'run/:run_id/datapoints', to: 'datapoints#add'
   end
 end
