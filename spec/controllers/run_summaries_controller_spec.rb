9a5b7a4f840f2109a79c92463ccf253335e5d3ff
Ari Kardasis <ari.kardasis@gmail.com>
2022-07-12 08:30:59 -0400

    fix mis-named controller spec

--- spec/controllers/run_summaries_controller_spec.rb
+++ spec/controllers/run_summaries_controller_spec.rb
@@ -7,7 +7,6 @@ describe RunSummariesController do
     it 'returns a 200' do
       get :index
       expect(response.status).to eq(200)
-      expect(response).to render_template(:run_summaries)
     end
   end
 end
