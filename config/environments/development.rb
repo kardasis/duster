a75c50f3090bf0280b16791ce29ca720f6189f42
Ari Kardasis <ari.kardasis@gmail.com>
2022-07-13 21:20:09 -0400

    allow traffic from local network in dev.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  #
+  # For testing on local network
+  config.web_console.whitelisted_ips = '192.168.0.0/16'
 end
