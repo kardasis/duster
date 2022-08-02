058ae368d3197845b286f31c0eb989ce87fc8848
Ari Kardasis <ari.kardasis@gmail.com>
2022-08-02 12:03:29 -0400

    fix duplication test

--- spec/controllers/run_summaries_controller_spec.rb
+++ spec/controllers/run_summaries_controller_spec.rb
@@ -18,13 +18,17 @@ describe RunSummariesController do
 
   describe 'duplication' do
     it 'should duplicate the run_summary' do
+      allow(Time).to receive(:now).and_return(Time.new(2002, 10, 31, 2, 2, 2).utc)
+
       rs = create :run_summary
+
+      allow(Time).to receive(:now).and_return(Time.new(2012, 11, 1, 4, 4, 4).utc)
 
       post :duplicate, params: { run_summary_id: rs.id }
 
       json_response = JSON.parse response.body
       expect(json_response[:id]).not_to eq rs.id
-      expect(json_response['start_time']).to eq(rs.start_time.iso8601(3))
+      expect(json_response['start_time']).not_to eq(rs.start_time.iso8601(3))
       expect(json_response['total_distance'].to_f).to eq(rs.total_distance.to_f)
       expect(json_response['total_time'].to_f).to eq(rs.total_time.to_f)
     end
